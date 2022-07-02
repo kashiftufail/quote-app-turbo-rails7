f112c7e10800fd4a2a4c46c536a2d093fdcf5c82
kashif tufail <kashif.tufail17@gmail.com>
2022-07-02 23:19:19 +0500

    add turbo for nested forms

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,8 +1,23 @@
 class Quote < ApplicationRecord
   validates :name, presence: true
   scope :ordered, -> { order(id: :desc) }
+  belongs_to :company
+  has_many :line_item_dates, dependent: :destroy
 
-  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+
+    # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+#   after_create_commit -> { broadcast_prepend_to "quotes" }
+#   after_update_commit -> { broadcast_replace_to "quotes" }
+#   after_destroy_commit -> { broadcast_remove_to "quotes" }
+  
+    #asynchronius
+
+#   after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+#   or
+
+broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  # get 'pages/home'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "quotes#index"
-  resources :quotes
+  root to: "pages#home"
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
 end
